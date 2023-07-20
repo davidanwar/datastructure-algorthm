d0b3e0e6f8196d3a3ed4426e750c33d9c625004d
David Anwar <david.anwar@ifg-life.id>
2023-07-20 08:38:13 +0700

    fibonacci

--- src/recursive/Recursive.java
+++ src/recursive/Recursive.java
@@ -5,12 +5,28 @@ public class Recursive {
         Recursive recursive = new Recursive();
         final int factorial = recursive.factorial(3);
         System.out.println(factorial);
+
+        final int fibonacci = recursive.fibonacci(4);
+        System.out.println("Fibonacci: " + fibonacci);
     }
 
     public int factorial(int n) {
+        if (n < 0) {
+            return -1;
+        }
         if (n == 1 || n == 0) {
             return 1;
         }
         return n * factorial(n-1);
+    }
+
+    public int fibonacci(int n) {
+        if (n < 0) {
+            return -1;
+        }
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
