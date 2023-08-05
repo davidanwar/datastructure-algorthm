f49c678fc4008bc47cb80679f10efeff48c9637b
David Anwar <david.anwar@ifg-life.id>
2023-08-05 16:45:05 +0700

    traverse LinkedList

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -4,7 +4,15 @@ public class LinkedList {
     public static void main(String[] args) {
         SingleLinkedList sLL = new SingleLinkedList();
         sLL.createSingleLinkedLIst(5);
-        System.out.println(sLL.head.value);
+        sLL.insertLinkedList(6, 0);
+        sLL.insertLinkedList(7, 1);
+        sLL.insertLinkedList(8, 1);
+        sLL.insertLinkedList(9, 1);
+        sLL.insertLinkedList(10, 2);
+        sLL.traverseSingleLinkedList();
+//        sLL.insertLinkedList(9, 1);
+//        System.out.println(sLL.head.next.next.value);
+
     }
 }
 
--- src/linkedlist/SingleLinkedList.java
+++ src/linkedlist/SingleLinkedList.java
@@ -12,6 +12,50 @@ public class SingleLinkedList {
         node.value = nodeValue;
         head = node;
         tail = node;
+        size = 1;
         return head;
+    }
+
+    public void insertLinkedList(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null) {
+            createSingleLinkedLIst(nodeValue);
+            return;
+        } else if (location == 0) {
+            node.next = head;
+            head = node;
+        } else if (location >= size) {
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
+    }
+
+    public void traverseSingleLinkedList() {
+        if (head == null) {
+            System.out.println("Single Linked List Does Not Exist");
+        } else {
+            Node tempNode = head;
+            for (int i = 0;  i < size; i++) {
+                System.out.print(tempNode.value);
+                if (i != size - 1) {
+                    System.out.print(" -> ");
+                }
+                tempNode = tempNode.next;
+            }
+        }
+        System.out.println("\n");
     }
 }
